4c247ebef7960f6c03e32df796c28f039e44eca7
jsonay <jsonay@163.com>
2014-11-05 22:28:43 +0800

    first commit

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#encoding = utf-8
+
+case $1 in
+start)
+     nohup python  manage.py runserver 0.0.0.0:8080  &
+     echo "server is start on 0.0.0.0:8080"
+     ;;
+stop)
+     ps -ef |awk '/0.0.0.0:8080/&& !/awk/ {print $2}'|xargs kill -9
+     ;;
+*)
+     echo "Usage $0 {start|stop}"
+     ;;
+esac
